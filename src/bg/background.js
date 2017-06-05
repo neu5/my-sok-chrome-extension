11b2eaab6a9baea0596ab7dcead562ad2dbfddbd
Jakub Kasprzyk <neus90@gmail.com>
2017-06-05 17:46:31 +0200

    WIP: send data to dev serv

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,9 +1,17 @@
 sendSelection = function(pageObj) {
   var s = pageObj.selectionText;
   var pageUrl = pageObj.pageUrl;
-  var query = '?s=' + s + '&url=' + pageUrl;
+  var query = 'http://localhost:9020/add' + '?s=' + s + '&url=' + pageUrl;
   
-  // @todo: send query to the server
+  //  chrome.tabs.create({url: query});
+  // var jax = new XMLHttpRequest();
+  // jax.open("GET","http://localhost:9020/add");
+  // jax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+  // jax.send("text=" + seltext);
+  // jax.onreadystatechange = function() { if(jax.readyState==4) { alert(jax.responseText);  }}
+  var browserRequest = new XMLHttpRequest();
+  browserRequest.open('GET', query, true);
+  browserRequest.sned(null);
 };
 
 chrome.contextMenus.create({
